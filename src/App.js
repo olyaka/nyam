ee0dcb65e36be4eb21c83fab63ef25b99f34ee66
Olga <fej-d@yandex.ru>
2018-10-21 14:45:19 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,132 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-class App extends Component {
+function Comment(props) {
+  const status = props.status;
+  const comment = props.comment;
+  const ingredient = props.description.ingredient;
+  if (status === "default") {
+    return <DefaultComment comment={comment.default} />;
+  } else if (status === "disabled") {
+    return <DisabledComment ingredient={ingredient} />;
+  } else if (status === "selected") {
+    return <SelectedComment comment={comment.selected} />;
+  }
+}
+
+function DefaultComment(props) {
+  return (
+    <div className="comment">
+      {props.comment.text}
+      <a>{props.comment.link}</a>
+    </div>
+  );
+}
+
+function DisabledComment(props) {
+  return (
+    <div className="comment disabled">
+      Печалька, {props.ingredient} закончился
+    </div>
+  );
+}
+
+function SelectedComment(props) {
+  return <div className="comment selected">{props.comment}</div>;
+}
+
+class Product extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: this.props.status
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    if (this.state.status !== "disabled") {
+      this.setState(state => ({
+        status: state.status === "default" ? "selected" : "default"
+      }));
+
+      const ctx = this.refs.canvas.getContext("2d");
+
+      ctx.strokeStyle === "#1698d9"
+        ? (ctx.strokeStyle = "rgb(217, 22, 103)")
+        : (ctx.strokeStyle = "#1698d9");
+      ctx.stroke();
+
+      e.currentTarget.addEventListener("mouseout", function() {
+        if (this.classList.contains("selected")) {
+          this.classList.add("out");
+        }
+      });
+    }
+  }
+  componentDidMount() {
+    this.updateCanvas();
+  }
+  updateCanvas() {
+    const ctx = this.refs.canvas.getContext("2d");
+    ctx.fillStyle = "rgb(242, 242, 242)";
+    ctx.strokeStyle = "rgb(22, 152, 217)";
+    ctx.lineWidth = 6;
+    ctx.beginPath();
+    ctx.moveTo(0, 45);
+    ctx.lineTo(45, 0);
+    ctx.lineTo(312, 0);
+    ctx.lineTo(312, 472);
+    ctx.lineTo(0, 472);
+    ctx.closePath();
+
+    ctx.fill();
+    ctx.stroke();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div
+        className={"item " + this.state.status}
+        onClick={e => this.handleClick(e)}
+      >
+        <canvas id="canvas" ref="canvas" width="312" height="472" />
+        <div className="overlay" />
+        <div className="container">
+          <p className="dish">{this.props.description.dish}</p>
+          <div className="product-title">{this.props.description.title}</div>
+          <div className="ingredient">{this.props.description.ingredient}</div>
+          <div className="portions">
+            {this.props.description.portions} порций
+          </div>
+          <div className="gift">{this.props.description.gift}</div>
+          <div className="cat-pic-container" />
+          <div className="weight">
+            {this.props.weight}
+            <br />
+            <span>кг</span>
+          </div>
+          <Comment
+            status={this.state.status}
+            comment={this.props.comment}
+            description={this.props.description}
+          />
+        </div>
       </div>
     );
   }
 }
 
+const App = ({ title, products }) => (
+  <div className="bg">
+    <div className="title">{title}</div>
+    <div className="products">
+      {products.map((product, i) => (
+        <Product key={i} {...product} />
+      ))}
+    </div>
+  </div>
+);
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var title = "Ты сегодня покормил кота?";
+var data = [
+  {
+    status: "default",
+    description: {
+      dish: "Сказочное заморское яство",
+      title: "Нямушка",
+      ingredient: "с фуа-гра",
+      portions: 10,
+      gift: "мышь в подарок"
+    },
+    weight: 0.5,
+    comment: {
+      default: {
+        text: "Чего сидишь? Порадуй котэ, ",
+        link: "купи."
+      },
+      selected: "Печень утки разварная с артишоками."
+    }
+  },
+  {
+    status: "default",
+    description: {
+      dish: "Сказочное заморское яство",
+      title: "Нямушка",
+      ingredient: "с рыбой",
+      portions: 40,
+      gift: "2 мыши в подарок"
+    },
+    weight: 2,
+    comment: {
+      default: {
+        text: "Чего сидишь? Порадуй котэ, ",
+        link: "купи."
+      },
+      selected: "Головы щучьи с чесноком да свежайшая сёмгушка."
+    }
+  },
+  {
+    status: "disabled",
+    description: {
+      dish: "Сказочное заморское яство",
+      title: "Нямушка",
+      ingredient: "с курой",
+      portions: 100,
+      gift: "5 мышей в подарок"
+    },
+    weight: 5,
+    comment: {
+      default: {
+        text: "Чего сидишь? Порадуй котэ, ",
+        link: "купи."
+      },
+      selected: "Филе из цыплят с трюфелями в бульоне."
+    }
+  }
+];
+
+ReactDOM.render(
+  <App title={title} products={data} />,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
