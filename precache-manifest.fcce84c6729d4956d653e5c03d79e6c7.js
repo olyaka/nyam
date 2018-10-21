eda4aacfc1ccd7f7aabdeede378ca52eb6d7d1e2
Olga <fej-d@yandex.ru>
2018-10-21 14:53:42 +0300

    Updates

---
+++ precache-manifest.fcce84c6729d4956d653e5c03d79e6c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nyam/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "00b5875d3614e42e89425d89d84c9d14",
+    "url": "/nyam/static/media/Pattern.00b5875d.jpg"
+  },
+  {
+    "revision": "ef1c6577ce0935c10b1a49ea7e33a64f",
+    "url": "/nyam/static/media/Cat.ef1c6577.png"
+  },
+  {
+    "revision": "3661e97d3f799267a20a",
+    "url": "/nyam/static/js/runtime~main.3661e97d.js"
+  },
+  {
+    "revision": "cf015c596ea821472ab6",
+    "url": "/nyam/static/js/main.cf015c59.chunk.js"
+  },
+  {
+    "revision": "ccf717e8f1c052517a1a",
+    "url": "/nyam/static/js/1.ccf717e8.chunk.js"
+  },
+  {
+    "revision": "cf015c596ea821472ab6",
+    "url": "/nyam/static/css/main.fbad1b25.chunk.css"
+  },
+  {
+    "revision": "e6b38e38956efdb6525d02f62ba2e5ed",
+    "url": "/nyam/index.html"
+  }
+];
